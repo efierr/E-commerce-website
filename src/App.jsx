73110456e1032fcfbdf3c546ec3def139e2e56aa
efierr <erickfierro0413@gmail.com>
2025-04-30 20:08:35 -0400

    created navbar and footer component to begin draft on e-commerce site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
-
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 function App() {
   return (
     <>
       <div>
-        <p>Hello World !</p>
+        <Navbar />
+        <div>CONTENT</div>
+        <Footer />
       </div>
     </>
   );
